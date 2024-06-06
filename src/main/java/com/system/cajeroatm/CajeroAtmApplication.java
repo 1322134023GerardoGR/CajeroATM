06facc27edbf5c09be4ee5241ec3a63ced44ca9c
GerardoGR <gerardo_1322134023@uptecamac.edu.mx>
2024-06-06 12:41:43 -0600

    build: first commit

---
+++ src/main/java/com/system/cajeroatm/CajeroAtmApplication.java
@@ -0,0 +1,13 @@
+package com.system.cajeroatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajeroAtmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CajeroAtmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/system/cajeroatm/CajeroAtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.cajeroatm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajeroAtmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
